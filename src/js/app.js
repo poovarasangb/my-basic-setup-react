18f49db225b7f6c4b85d80c43a36a51cbde3d660
poovarasangb <poovarasanit13@gmail.com>
2023-05-10 09:07:16 +0530

    Changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,15 @@
 import { lazy, Suspense } from "react";
-import { useTime } from "./customHook/time";
 
 const DisplayTime = lazy(() => import("./displayTime"));
 
 export const App = ({
     time = new Date().getTime()
-}) => {
-    const [date] = useTime(time);
-    return (
-        <>
-            <Suspense fallback={null}>
-                <DisplayTime
-                    dateProps={time}
-                />
-            </Suspense>
-            <h1>
-                {"Using customHook ==> "}
-                {`${date.toLocaleDateString() } :: ${ date.toLocaleTimeString()}`}
-            </h1>
-
-        </>
-    );
-};
+}) => (
+    <>
+        <Suspense fallback={null}>
+            <DisplayTime
+                dateProps={time}
+            />
+        </Suspense>
+    </>
+);
--- src/js/displayTime.tsx
+++ src/js/displayTime.tsx
@@ -10,7 +10,7 @@ const DisplayTime = ({
     const [date] = useTime(dateProps);
     return (
         <h1 className="red">
-            {`Using component ==> ${ date }`}
+            {`${date.toLocaleDateString() }::${ date.toLocaleTimeString()}`}
         </h1>
     );
 };
